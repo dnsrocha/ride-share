909ac9674b843929c3498860dd6d14087c5834bd
dnsrocha <drocha@gmail.com>
2020-09-12 17:51:12 -0700

    all answers done needs to improve output

--- worksheet.rb
+++ worksheet.rb
@@ -35,45 +35,45 @@
 #
 
 drivers = {"DR0001": {
-    "3rd Feb 2016": [
-        {cost: 10, rider_id: "RD0003", rating: 3},
-        {cost: 30, rider_id: "RD0015", rating: 4}
-    ],
-    "5th Feb 2016": [
-        {cost: 45, rider_id: "RD0003", rating: 2}
-    ]
-},
+                        "3rd Feb 2016": [
+                            {cost: 10, rider_id: "RD0003", rating: 3},
+                            {cost: 30, rider_id: "RD0015", rating: 4}
+                        ],
+                        "5th Feb 2016": [
+                             {cost: 45, rider_id: "RD0003", rating: 2}
+                        ]
+                      },
            "DR0002": {
-               "3rd Feb 2016": [
-                   {cost: 25, rider_id: "RD0073", rating: 5}
-               ],
-               "4th Feb 2016": [
-                   {cost: 15, rider_id: "RD0013", rating: 1}
-               ],
-               "5th Feb 2016": [
-                   {cost: 35, rider_id: "RD0066", rating: 3}
-               ]
-           },
+                        "3rd Feb 2016": [
+                            {cost: 25, rider_id: "RD0073", rating: 5}
+                        ],
+                        "4th Feb 2016": [
+                            {cost: 15, rider_id: "RD0013", rating: 1}
+                        ],
+                        "5th Feb 2016": [
+                            {cost: 35, rider_id: "RD0066", rating: 3}
+                        ]
+                      },
            "DR0003": {
-               "4th Feb 2016": [
-                   {cost: 5, rider_id: "RD0066", rating: 5},
-               ],
-               "5th Feb 2016": [
-                   {cost: 50, rider_id: "RD0003", rating: 2}
-               ]
-           },
+                        "4th Feb 2016": [
+                            {cost: 5, rider_id: "RD0066", rating: 5},
+                        ],
+                        "5th Feb 2016": [
+                            {cost: 50, rider_id: "RD0003", rating: 2}
+                        ]
+                      },
            "DR0004": {
-               "3rd Feb 2016": [
-                   {cost: 5, rider_id: "RD0022", rating: 5}
-               ],
-               "4th Feb 2016": [
-                   {cost: 10, rider_id: "RD0022", rating: 4}
-               ],
-               "5th Feb 2016": [
-                   {cost: 20, rider_id: "RD0073", rating: 5}
-               ]
-           }
-}
+                        "3rd Feb 2016": [
+                            {cost: 5, rider_id: "RD0022", rating: 5}
+                        ],
+                        "4th Feb 2016": [
+                            {cost: 10, rider_id: "RD0022", rating: 4}
+                        ],
+                        "5th Feb 2016": [
+                            {cost: 20, rider_id: "RD0073", rating: 5}
+                        ]
+                      }
+          }
 
 #The number of rides each driver has given
 def rides(drivers)
@@ -149,3 +149,52 @@ end
 
 puts "\n"
 richest(drivers)
+
+#Which driver has the highest average rating?
+
+def best_rating(drivers)
+  highest_average = []
+  drivers.each do |driver, data|
+    av_rates = []
+    data.map do |day, rides|
+      rides.each do |ride|
+        av_rates.push ride[:rating]
+      end
+    end
+    highest_average.push av_rates.sum
+
+  end
+  printf("%.1f\n", highest_average.max)
+end
+
+puts "\n"
+best_rating(drivers)
+
+
+#For each driver, on which day did they make the most money?
+def most_money(drivers)
+
+  drivers.each do |driver, data|
+    cash = []
+    which_day = []
+    data.map do |day, rides|
+      total = []
+      rides.each do |ride|
+        total.push ride[:cost]
+      end
+
+      cash.push total.sum
+      which_day.push day
+    end
+
+    if cash.include?(cash.max)
+      where = cash.index(cash.max)
+      puts "On #{which_day[where]}, driver #{driver} made the most money."
+      puts cash.max
+    end
+  end
+
+end
+
+puts "\n"
+most_money(drivers)
